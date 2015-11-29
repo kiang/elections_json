b518a7e21c73f53cdd9865ba01e9f256e3690951
kiang <kiange@gmail.com>
2015-11-30 06:54:33 +0800

    script to commit media

---
+++ media_commit.php
@@ -0,0 +1,20 @@
+<?php
+
+$path = __DIR__;
+
+$now = date('Y-m-d H:i:s');
+$count = $size = 0;
+// 50MB = 52428800
+foreach (glob($path . '/media/*/*/*/*/*.*') AS $imgFile) {
+    system("/usr/bin/git add {$imgFile}");
+    $size += filesize($imgFile);
+    if ($size >= 52428800) {
+        ++$count;
+        system("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'media commit - {$count} @ {$now}'");
+        system("cd {$path} && /usr/bin/git push origin master");
+        $size = 0;
+    }
+}
+
+system("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'media commit - {$count} @ {$now}'");
+system("cd {$path} && /usr/bin/git push origin master");
